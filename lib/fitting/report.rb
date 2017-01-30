f9f980277256d6486353b9cf29a14012fecca3e5
d.efimov <d.efimov@fun-box.ru>
2017-01-30 14:59:46 +0400

    save list documented type

--- lib/fitting/report.rb
+++ lib/fitting/report.rb
@@ -1,8 +1,21 @@
 module Fitting
   class Report
     def initialize(tests)
+      documented = {}
+      not_documented = {}
+      tests.map do |location, test|
+        if test['request']['schema'].nil?
+          not_documented["#{test['request']['method']} #{test['request']['path']}"] = {}
+        else
+          documented["#{test['request']['schema']['method']} #{test['request']['schema']['path']}"] = {}
+        end
+      end
       @json = {
-        'tests' => tests
+        'tests' => tests,
+        'requests' => {
+          'documented' => documented,
+          'not_documented' => not_documented
+        }
       }
     end
 
