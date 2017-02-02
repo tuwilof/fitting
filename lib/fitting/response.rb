25ba7147ca44efd5e24c0252598455a55980ecd7
d.efimov <d.efimov@fun-box.ru>
2017-02-02 17:09:09 +0400

    refactoring set_fully_validate

--- lib/fitting/response.rb
+++ lib/fitting/response.rb
@@ -9,20 +9,20 @@ module Fitting
       @status = env_response.status
       @body = env_response.body
       @schemas = expect_request.find_responses(status: @status) if expect_request
-      valid if @schemas
+      @schemas = set_fully_validate if @schemas
       raise Response::NotDocumented unless (@schemas&.first) || Fitting.configuration.skip_not_documented
       self
     end
 
-    def valid
+    def set_fully_validate
       @valid = false
-      @schemas = @schemas.map do |doc_response|
-        fully_validate = JSON::Validator.fully_validate(doc_response['body'], @body)
-        doc_response = doc_response.merge('fully_validate' => fully_validate)
+      @schemas.map do |old_schema|
+        fully_validate = JSON::Validator.fully_validate(old_schema['body'], @body)
+        new_schema = old_schema.merge('fully_validate' => fully_validate)
         if fully_validate == []
           @valid = true
         end
-        doc_response
+        new_schema
       end
     end
 
