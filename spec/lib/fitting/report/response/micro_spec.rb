964017360b80232ecb37adcfa9ba562c33f502b3
d.efimov <d.efimov@fun-box.ru>
2017-02-02 15:19:55 +0400

    improved description for tests

--- spec/lib/fitting/report/response/micro_spec.rb
+++ spec/lib/fitting/report/response/micro_spec.rb
@@ -4,28 +4,30 @@ require 'multi_json'
 
 RSpec.describe Fitting::Report::Response::Micro do
   describe '.new' do
-    it do
-      described_class.new(
-        {
-          'location1' => {
-            'request' => MultiJson.dump({}),
-            'response' => MultiJson.dump({})
-          },
-          'location2' => {
-            'request' => MultiJson.dump({'schema' => {}}),
-            'response' => MultiJson.dump({})
-          },
-          'location3' => {
-            'request' => MultiJson.dump({'schema' => {}}),
-            'response' => MultiJson.dump({'schemas' => {}})
+    it 'not raise exception' do
+      expect do
+        described_class.new(
+          {
+            'location1' => {
+              'request' => MultiJson.dump({}),
+              'response' => MultiJson.dump({})
+            },
+            'location2' => {
+              'request' => MultiJson.dump({'schema' => {}}),
+              'response' => MultiJson.dump({})
+            },
+            'location3' => {
+              'request' => MultiJson.dump({'schema' => {}}),
+              'response' => MultiJson.dump({'schemas' => {}})
+            }
           }
-        }
-      )
+        )
+      end.not_to raise_exception
     end
   end
 
   describe '#responses_documented' do
-    it do
+    it 'not raise exception' do
       response = {
         'schemas' => [
           {
@@ -34,10 +36,12 @@ RSpec.describe Fitting::Report::Response::Micro do
           }
         ]
       }
-      described_class.new({}).responses_documented('location', {}, response)
+      expect do
+        described_class.new({}).responses_documented('location', {}, response)
+      end.not_to raise_exception
     end
 
-    it do
+    it 'not raise exception' do
       response = {
         'valid' => true,
         'schemas' => [
@@ -47,13 +51,17 @@ RSpec.describe Fitting::Report::Response::Micro do
           }
         ]
       }
-      described_class.new({}).responses_documented('location', {}, response)
+      expect do
+        described_class.new({}).responses_documented('location', {}, response)
+      end.not_to raise_exception
     end
   end
 
   describe '#to_hash' do
-    it do
-      described_class.new({}).to_hash
+    it 'not raise exception' do
+      expect do
+        described_class.new({}).to_hash
+      end.not_to raise_exception
     end
   end
 end
