d6aefd6d10d2745c4c41a12ec9d4401959f9247e
d.efimov <d.efimov@fun-box.ru>
2017-02-16 14:47:07 +0400

    reports without formatter

--- lib/fitting.rb
+++ lib/fitting.rb
@@ -2,6 +2,10 @@ require 'fitting/version'
 require 'fitting/documentation'
 require 'fitting/configuration'
 
+require 'yaml'
+require 'fitting/storage/yaml_file'
+require 'fitting/report/response/macro'
+
 module Fitting
   class << self
     def configure
@@ -23,6 +27,29 @@ module ActionController
         response = origin_process(action, *args)
         Fitting::Documentation.try_on(self, request, response)
         response
+      end
+    end
+  end
+end
+
+module RSpec
+  module Core
+    # Provides the main entry point to run a suite of RSpec examples.
+    class Runner
+      alias origin_run_specs run_specs
+
+      def run_specs(example_groups)
+        Fitting::Storage::YamlFile.craft
+
+        origin_run_specs(example_groups)
+
+        tests = Fitting::Storage::YamlFile.load
+        Fitting::Storage::YamlFile.destroy
+
+        report = Fitting::Report::Response::Macro.new(tests).to_hash
+        File.open('report_response_macro.yaml', 'w') do |file|
+          file.write(YAML.dump(report))
+        end
       end
     end
   end
