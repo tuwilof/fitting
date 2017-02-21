140ac90959aa94cd75e250ff2a949172c2b1b812
d.efimov <d.efimov@fun-box.ru>
2017-02-21 12:43:31 +0400

    add routes for documentation

--- lib/fitting/documentation.rb
+++ lib/fitting/documentation.rb
@@ -6,6 +6,27 @@ module Fitting
       def tomogram
         @tomogram ||= TomogramRouting::Tomogram.craft(Fitting.configuration.tomogram)
       end
+
+      def routes
+        routes = {}
+        MultiJson.load(Fitting.configuration.tomogram).map do |request|
+          responses = {}
+          request['responses'].map do |response|
+            unless responses[response['status']]
+              responses[response['status']] = 0
+            end
+            responses[response['status']] += 1
+          end
+
+          responses.map do |response|
+            response.last.times do |index|
+              route = "#{request['method']} #{request['path']} #{response.first} #{index}"
+              routes[route] = nil
+            end
+          end
+        end
+        routes.keys
+      end
     end
   end
 end
--- lib/fitting/report/response.rb
+++ lib/fitting/report/response.rb
@@ -7,35 +7,15 @@ module Fitting
         coverage = coverage_routes(responses)
         @json = {
           'coverage' => coverage,
-          'not coverage' => documented - coverage
+          'not coverage' => Fitting::Documentation.routes - coverage
         }
         puts "Coverage documentations API by RSpec tests: #{percent_covered(responses)}%"
       end
 
       def percent_covered(tests)
         covered = coverage_routes(tests).size.to_f
-        all = documented.size.to_f
+        all = Fitting::Documentation.routes.size.to_f
         (covered / all * 100.0).round(2)
-      end
-
-      def documented
-        routes = {}
-        MultiJson.load(Fitting.configuration.tomogram).map do |request|
-          responses = {}
-          request['responses'].map do |response|
-            unless responses[response['status']]
-              responses[response['status']] = 0
-            end
-            responses[response['status']] += 1
-          end
-
-          responses.map do |response|
-            response.last.times do |index|
-              routes["#{request['method']} #{request['path']} #{response.first} #{index}"] = nil
-            end
-          end
-        end
-        routes.keys
       end
 
       def coverage_routes(responses)
