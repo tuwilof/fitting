95d5036aaea4d2290dc4597c1bd577ebecdf139e
d.efimov <d.efimov@fun-box.ru>
2017-03-15 12:28:17 +0400

    improve statistics for fitting

--- lib/fitting/statistics.rb
+++ lib/fitting/statistics.rb
@@ -7,29 +7,22 @@ require 'fitting/documentation/statistics_with_conformity_lists'
 module Fitting
   class Statistics
     def initialize(response_routes, response_route_white)
-      @response_routes = response_routes
-      @response_route_white = response_route_white
+      response_route_black = Fitting::Documentation::Response::Route.new(Fitting::Storage::Responses.all, response_routes.black)
+      request_route_black = Fitting::Documentation::Request::Route.new(response_route_black)
+      request_route_white = Fitting::Documentation::Request::Route.new(response_route_white)
+      @statistics = Fitting::Documentation::Statistics.new(request_route_black, response_routes.black, response_route_black)
+      @statistics_with_conformity_lists = Fitting::Documentation::StatisticsWithConformityLists.new(request_route_white, response_routes.white, response_route_white)
     end
 
     def to_s
       if Fitting.configuration.white_list
-        response_route_black = Fitting::Documentation::Response::Route.new(
-          Fitting::Storage::Responses.all,
-          @response_routes.black
-        )
-
-        request_route = Fitting::Documentation::Request::Route.new(response_route_black)
-        statistics = Fitting::Documentation::Statistics.new(request_route, @response_routes.black, response_route_black)
-
-        str_statistics =[
-          ['[Black list]', statistics].join("\n"),
+        str_statistics = [
+          ['[Black list]', @statistics].join("\n"),
           '[White list]'
         ].join("\n\n")
       end
-      request_route = Fitting::Documentation::Request::Route.new(@response_route_white)
-      statistics_with_conformity_lists = Fitting::Documentation::StatisticsWithConformityLists.new(request_route, @response_routes.white, @response_route_white)
 
-      [str_statistics, statistics_with_conformity_lists].compact.join("\n")
+      [str_statistics, @statistics_with_conformity_lists, "\n"].compact.join("\n")
     end
   end
 end
--- spec/lib/fitting/statistics_spec.rb
+++ spec/lib/fitting/statistics_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Fitting::Statistics do
   describe '#to_s' do
-    let(:response_routes) { double(white: nil) }
+    let(:response_routes) { double(white: nil, black: nil) }
     let(:response_route_white) { nil }
 
     subject { described_class.new(response_routes, response_route_white) }
@@ -13,12 +13,10 @@ RSpec.describe Fitting::Statistics do
     end
 
     it 'returns statistics' do
-      expect(subject.to_s).to eq('statistics_with_conformity_lists')
+      expect(subject.to_s).to eq("statistics_with_conformity_lists\n\n")
     end
 
     context 'uses whitelist' do
-      let(:response_routes) { double(white: nil, black: nil) }
-
       before do
         allow(Fitting.configuration).to receive(:white_list).and_return([])
         allow(Fitting::Documentation::Response::Route).to receive(:new)
@@ -29,7 +27,8 @@ RSpec.describe Fitting::Statistics do
         expect(subject.to_s).to eq(
           [
             ['[Black list]', 'statistics'].join("\n"),
-            ['[White list]', 'statistics_with_conformity_lists'].join("\n")
+            ['[White list]', 'statistics_with_conformity_lists'].join("\n"),
+            ""
           ].join("\n\n")
         )
       end
