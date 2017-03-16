9d99dba7a3a4df7696612a18d06ce959f31b05c4
d.efimov <d.efimov@fun-box.ru>
2017-03-16 11:17:30 +0400

    improve white_list

--- lib/fitting/documentation/statistics.rb
+++ lib/fitting/documentation/statistics.rb
@@ -6,10 +6,10 @@ module Fitting
   module Documentation
     class Statistics
       def initialize(documentation, white_list, all_responses)
-        routes = Fitting::Documentation::Response::Routes.new(documentation, white_list)
-        @monochrome_route = Fitting::Documentation::Response::MonochromeRoute.new(all_responses, routes)
-        @statistics = Fitting::Documentation::Route.new(routes.black, @monochrome_route.black).statistics
-        @statistics_with_conformity_lists = Fitting::Documentation::Route.new(routes.white, @monochrome_route.white).statistics_with_conformity_lists
+        @routes = Fitting::Documentation::Response::Routes.new(documentation, white_list)
+        @monochrome_route = Fitting::Documentation::Response::MonochromeRoute.new(all_responses, @routes)
+        @statistics = Fitting::Documentation::Route.new(@routes.black, @monochrome_route.black).statistics
+        @statistics_with_conformity_lists = Fitting::Documentation::Route.new(@routes.white, @monochrome_route.white).statistics_with_conformity_lists
       end
 
       def not_coverage?
@@ -17,7 +17,7 @@ module Fitting
       end
 
       def to_s
-        if Fitting.configuration.white_list
+        if @routes.black.any?
           [
             ['[Black list]', @statistics].join("\n"),
             ['[White list]', @statistics_with_conformity_lists].join("\n"),
