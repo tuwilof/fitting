9147e35c83f6b1841bac92b5b515d335ea5ab1b3
d.efimov <d.efimov@fun-box.ru>
2017-03-22 14:55:31 +0400

    cover documentation

---
+++ spec/lib/fitting/documentation_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'multi_json'
+
+RSpec.describe Fitting::Documentation do
+  let(:tomogram) { MultiJson.dump([{'responses' => [{'status' => 200}]}]) }
+  let(:white_list) { nil }
+
+  subject { described_class.new(tomogram, white_list) }
+
+  describe '#black' do
+    it 'returns empty array' do
+      expect(subject.black).to eq([])
+    end
+
+    context 'has whitelist' do
+      let(:white_list) { {} }
+
+      it 'returns responses array' do
+        expect(subject.black).to eq(["\t 200 0"])
+      end
+    end
+  end
+
+  describe '#white' do
+    it 'returns responses array' do
+      expect(subject.white).to eq(["\t 200 0"])
+    end
+
+    context 'has whitelist' do
+      let(:white_list) { {} }
+
+      it 'returns empty array' do
+        expect(subject.white).to eq([])
+      end
+    end
+  end
+
+  describe '#all' do
+    it 'returns responses array' do
+      expect(subject.all).to eq(["\t 200 0"])
+    end
+  end
+end
