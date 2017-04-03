8f42e4ec63657bade5aef5460d97355dc99d4d38
d.efimov <d.efimov@fun-box.ru>
2017-04-03 12:14:24 +0400

    fix parse request path

--- lib/fitting/request.rb
+++ lib/fitting/request.rb
@@ -4,7 +4,7 @@ module Fitting
   class Request
     def initialize(env_request, tomogram)
       @method = env_request.request_method
-      @path = env_request.env['PATH_INFO']
+      @path = env_request.env['PATH_INFO'] || env_request.fullpath
       @body = env_request.env['action_dispatch.request.request_parameters']
       @schema = tomogram.find_request(method: @method, path: @path)
       @fully_validate = JSON::Validator.fully_validate(@schema['request'], @body) if @schema
