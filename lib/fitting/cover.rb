00ea627bcbb8ad5b74235d59f28d188e6db70feb
d.efimov <d.efimov@fun-box.ru>
2017-06-16 15:37:38 +0400

    add to_hash for cover

--- lib/fitting/cover.rb
+++ lib/fitting/cover.rb
@@ -3,6 +3,14 @@ module Fitting
     def initialize(all_responses, coverage)
       @all_responses = all_responses
       @coverage = coverage
+      @list = {}
+    end
+
+    def to_hash
+      @all_responses.map do |response|
+        @list[response.route] = nil unless @list.has_key?(response.route)
+      end
+      @list
     end
 
     def save
--- spec/lib/fitting/cover_spec.rb
+++ spec/lib/fitting/cover_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe Fitting::Cover do
       expect { subject.save }.not_to raise_exception
     end
   end
+
+  describe '#to_hash' do
+    let(:all_responses) do
+      [
+        double(route: "POST\t/sessions 200 0"),
+        double(route: "POST\t/sessions 200 0")
+      ]
+    end
+    let(:coverage) do
+      [
+        "POST\t/sessions 200 0"
+      ]
+    end
+
+    it 'returns hash' do
+      expect(subject.to_hash).to eq({
+        "POST\t/sessions 200 0" => nil
+      })
+    end
+  end
 end
