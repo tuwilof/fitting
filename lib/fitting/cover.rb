f4d43b1a37e479b8dbb826d6656c22ba3360b323
d.efimov <d.efimov@fun-box.ru>
2017-06-27 18:29:56 +0400

    add in html pretty json_schema

--- lib/fitting/cover.rb
+++ lib/fitting/cover.rb
@@ -1,5 +1,6 @@
 require 'fitting/cover/response'
 require 'haml'
+require 'json'
 
 module Fitting
   class Cover
@@ -24,13 +25,24 @@ module Fitting
       @list
     end
 
+    def template
+      return @template if @template
+      @template = {}
+      to_hash.each do |key, value|
+        @template[key] = value
+        @template[key]['type'] = 'passed'
+        @template[key]['json_schema'] = JSON.pretty_generate(value['json_schemas'].last)
+      end
+      @template
+    end
+
     def save
       to_hash
       contents = File.read(File.expand_path('../view/report.html.haml', __FILE__))
       html = "<style>\n#{File.read(File.expand_path('../view/style.css', __FILE__))}\n</style>\n"
       html += Haml::Engine.new(contents).render(
         Object.new,
-        :@to_hash => to_hash
+        :@to_hash => template
       )
       html
     end
