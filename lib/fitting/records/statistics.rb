f3dadd84ee7c41a94a7da1934158ba8f57c5d278
d.efimov <d.efimov@fun-box.ru>
2017-07-17 21:14:37 +0400

    add main responses statistics

--- lib/fitting/records/statistics.rb
+++ lib/fitting/records/statistics.rb
@@ -3,6 +3,9 @@ module Fitting
     class Statistics
       def initialize(requests)
         @requests = requests
+        @all_responses = 0
+        @cover_responses = 0
+        @not_cover_responses = 0
       end
 
       def to_s(requests)
@@ -17,6 +20,8 @@ module Fitting
       end
 
       def statistics_with_conformity_lists
+        check_responses
+        @requests
         [
           'Fully conforming requests:',
           to_s(coverage_fully),
@@ -30,7 +35,25 @@ module Fitting
           "API requests with fully implemented responses: #{coverage_fully.size} (#{percent(@requests.size, coverage_fully.size)}% of #{@requests.size}).",
           "API requests with partially implemented responses: #{coverage_partially.size} (#{percent(@requests.size, coverage_partially.size)}% of #{@requests.size}).",
           "API requests with no implemented responses: #{coverage_non.size} (#{percent(@requests.size, coverage_non.size)}% of #{@requests.size}).",
+          '',
+          "API responses conforming to the blueprint: #{@cover_responses} (#{percent(@all_responses, @cover_responses)}% of #{@all_responses}).",
+          "API responses with validation errors or untested: #{@not_cover_responses} (#{percent(@all_responses, @not_cover_responses)}% of #{@all_responses})."
         ].join("\n")
+      end
+
+      def check_responses
+        @requests.to_a.map do |request|
+          request.responses.to_a.map do |response|
+            response.json_schemas.map do |json_schema|
+              if json_schema.bodies == []
+                @not_cover_responses += 1
+              else
+                @cover_responses += 1
+              end
+              @all_responses += 1
+            end
+          end
+        end
       end
 
       def coverage_fully
