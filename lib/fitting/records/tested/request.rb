1a7704b9e8b911d8edcb11a5f8482349381b458a
d.efimov <d.efimov@fun-box.ru>
2017-09-06 15:04:08 +0400

    improve and cover tested request

--- lib/fitting/records/tested/request.rb
+++ lib/fitting/records/tested/request.rb
@@ -5,13 +5,24 @@ module Fitting
   class Records
     class Tested
       class Request
-        attr_reader :method, :path, :body, :response
-
         def initialize(env_response)
-          @method = env_response.request.request_method
-          @path = Tomograph::Path.new(env_response.request.env['PATH_INFO'] || env_response.request.fullpath)
-          @body = env_response.request.env['action_dispatch.request.request_parameters']
-          @response = Fitting::Records::Tested::Response.new(env_response)
+          @env_response = env_response
+        end
+
+        def method
+          @method ||= @env_response.request.request_method
+        end
+
+        def path
+          @path ||= Tomograph::Path.new(@env_response.request.env['PATH_INFO'] || @env_response.request.fullpath)
+        end
+
+        def body
+          @body ||= @env_response.request.env['action_dispatch.request.request_parameters']
+        end
+
+        def response
+          @response ||= Fitting::Records::Tested::Response.new(@env_response)
         end
       end
     end
---
+++ spec/lib/fitting/records/tested/request_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'fitting/records/tested/request'
+
+RSpec.describe Fitting::Records::Tested::Request do
+  subject { described_class.new(env_response) }
+
+  describe '#method' do
+    let(:method) { double }
+    let(:env_response) { double(request: double(request_method: method)) }
+
+    it 'returns method' do
+      expect(subject.method).to eq(method)
+    end
+  end
+
+  describe '#path' do
+    let(:path) { double }
+    let(:env_response) { double(request: double(env: {'PATH_INFO' => path})) }
+
+    before { allow(Tomograph::Path).to receive(:new).and_return(path) }
+
+    it 'returns method' do
+      expect(subject.path).to eq(path)
+    end
+  end
+
+  describe '#body' do
+    let(:body) { double }
+    let(:env_response) { double(request: double(env: {'action_dispatch.request.request_parameters' => body})) }
+
+    it 'returns body' do
+      expect(subject.body).to eq(body)
+    end
+  end
+
+  describe '#response' do
+    let(:response) { double }
+    let(:env_response) { double }
+
+    it 'returns response' do
+      expect(subject.response).to be_a(Fitting::Records::Tested::Response)
+    end
+  end
+end
