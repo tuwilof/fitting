285014988033bced652f0cf14efb2d7060f83e54
d.efimov <d.efimov@fun-box.ru>
2017-09-06 11:07:46 +0400

    add unit response

---
+++ lib/fitting/records/unit/response.rb
@@ -0,0 +1,31 @@
+require 'fitting/records/unit/json_schema'
+
+module Fitting
+  class Records
+    class Unit
+      class Response
+        def initialize(documented_response, tested_responses)
+          @documented_response = documented_response
+          @tested_responses = tested_responses
+        end
+
+        def status
+          @status ||= @documented_response.status.to_s
+        end
+
+        def json_schemas
+          @json_schemas ||= @documented_response.json_schemas.inject([]) do |res, documented_json_schema|
+            res.push(Fitting::Records::Unit::JsonSchema.new(documented_json_schema, tested_bodies))
+          end
+        end
+
+        def tested_bodies
+          @tested_bodies ||= @tested_responses.inject([]) do |res, tested_response|
+            next res unless status == tested_response.status.to_s
+            res.push(tested_response.body)
+          end
+        end
+      end
+    end
+  end
+end
