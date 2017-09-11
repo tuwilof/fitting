633883dd95c3e4e1a761d75652dd8de3dc8e8078
d.efimov <d.efimov@fun-box.ru>
2017-09-11 16:24:15 +0400

    cover storage responses

---
+++ spec/lib/fitting/storage/responses_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'fitting/storage/responses'
+
+RSpec.describe Fitting::Storage::Responses do
+  describe '#add' do
+    before { allow(Fitting::Records::Tested::Request).to receive(:new) }
+
+    it 'does not raise an error' do
+      expect { subject.add(double) }.not_to raise_exception
+    end
+  end
+
+  describe '#statistics' do
+    before do
+      allow(Fitting::Statistics).to receive(:new)
+      allow(subject).to receive(:documented)
+    end
+
+    it 'does not raise an error' do
+      expect { subject.statistics }.not_to raise_exception
+    end
+  end
+
+  describe '#documented' do
+    before do
+      allow(Fitting::Records::Documented::Request).to receive(:new)
+      allow(subject).to receive(:tomogram).and_return([double])
+      allow(subject).to receive(:white_list).and_return(double(to_a: nil))
+    end
+
+    it 'does not raise an error' do
+      expect { subject.documented }.not_to raise_exception
+    end
+  end
+
+  describe '#tomogram' do
+    before { allow(Fitting::Storage::Documentation).to receive(:tomogram).and_return(double(to_hash: nil)) }
+
+    it 'does not raise an error' do
+      expect { subject.tomogram }.not_to raise_exception
+    end
+  end
+
+  describe '#white_list' do
+    before do
+      allow(Fitting::Storage::WhiteList).to receive(:new)
+      allow(Fitting).to receive(:configuration).and_return(double(white_list: nil, resource_white_list: nil))
+      allow(Fitting::Storage::Documentation).to receive(:tomogram).and_return(double(to_resources: nil))
+    end
+
+    it 'does not raise an error' do
+      expect { subject.white_list }.not_to raise_exception
+    end
+  end
+end
