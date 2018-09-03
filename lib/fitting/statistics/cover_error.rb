ff748050cb98f5aff2cd4e1520caeff6730fa752
d.efimov <d.efimov@fun-box.ru>
2018-09-03 22:31:51 +0400

    improve documentation_cover_error task

--- lib/fitting/statistics/cover_error.rb
+++ lib/fitting/statistics/cover_error.rb
@@ -1,14 +1,30 @@
 module Fitting
   class Statistics
     class CoverError
-      def initialize(measurement)
-        @measurement = measurement
+      def initialize(request_unit)
+        @request_unit = request_unit
       end
 
       def to_s
-        [
-          ''
-        ].compact.join("\n\n")
+        res = ''
+        @request_unit.map do |request|
+          request.responses.map do |response|
+            if response.tested_bodies == []
+              return 'Error, not all responses valid without cover!'
+            else
+              response.json_schemas.map do |json_schema|
+                json_schema.combinations.map do |combination|
+                  if combination.valid_bodies == []
+                    res += "request metohd#{request.method}\nrequest path:#{request.path}\n"\
+                            "response staus:#{response.status}\nsource json-schema#{json_schema.json_schema}\n"\
+                           "combination:#{combination.description}\nnew json-schema:#{combination.json_schema}\n\n"
+                  end
+                end
+              end
+            end
+          end
+        end
+        res
       end
     end
   end
