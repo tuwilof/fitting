6d9f4a180c2dea36290e677932c3613dd98f0f75
d.efimov <d.efimov@fun-box.ru>
2019-03-15 20:16:27 +0400

    improve test for enum

--- spec/lib/fitting/cover/json_schema_enum_spec.rb
+++ spec/lib/fitting/cover/json_schema_enum_spec.rb
@@ -4,129 +4,17 @@ require 'json-schema'
 require 'json'
 
 RSpec.describe Fitting::Cover::JSONSchemaEnum do
-  let(:json_schema) do
-    {
-      "$schema" => "http://json-schema.org/draft-04/schema#",
-      "type" => "object",
-      "properties" => {
-        "state" => {
-          "type" => "string",
-          "enum" => [
-            "ok",
-            "error"
-          ]
-        }
-      },
-      "required" => [
-        "state"
-      ]
-    }
-  end
-
-  subject { described_class.new(json_schema) }
-
-  describe '.new' do
-    it 'returns described class object' do
-      expect(subject).to be_a(described_class)
-    end
-  end
+  subject { described_class.new(original) }
 
   describe '#combi' do
-    it do
-      expect(subject.combi).to eq(
-        [
-          [
-            {
-              "$schema" => "http://json-schema.org/draft-04/schema#",
-              "type" => "object",
-              "properties" => {
-                "state" => {
-                  "type" => "string",
-                  "enum" => [
-                    "ok"
-                  ]
-                }
-              },
-              "required" => [
-                "state"
-              ]
-            },
-            %w[enum properties.state.ok]
-          ],
-          [
-            {
-              "$schema" => "http://json-schema.org/draft-04/schema#",
-              "type" => "object",
-              "properties" => {
-                "state" => {
-                  "type" => "string",
-                  "enum" => [
-                    "error"
-                  ]
-                }
-              },
-              "required" => [
-                "state"
-              ]
-            },
-            %w[enum properties.state.error]
-          ]
-        ]
-      )
-    end
-  end
+    let(:original) { MultiJson.load(File.read('spec/fixtures/enum/0.json')) }
+    let(:first_combination) { MultiJson.load(File.read('spec/fixtures/enum/1.json')) }
+    let(:second_combination) { MultiJson.load(File.read('spec/fixtures/enum/2.json')) }
+    let(:first_details) { %w[enum properties.state.ok] }
+    let(:second_details) { %w[enum properties.state.error] }
 
-  describe '#new_enum' do
-    it do
-      expect(subject.new_enum(
-        {
-          "type" => "string",
-          "enum" => [
-            "ok",
-            "error"
-          ]
-        }
-      )).to eq(
-        [
-          [
-            {
-              "type" => "string",
-              "enum" => [
-                "ok"
-              ]
-            },
-            %w[enum ok]
-          ],
-          [
-            {
-              "type" => "string",
-              "enum" => [
-                "error"
-              ]
-            },
-            %w[enum error]
-          ]
-        ]
-      )
-    end
-  end
-
-  describe "#new_keys" do
-    it do
-      expect(subject.new_keys(
-        {
-          "type" => "string",
-          "enum" => [
-            "ok",
-            "error"
-          ]
-        }
-      )).to eq(
-        [
-          "ok",
-          "error"
-        ]
-      )
+    it 'returns combinations' do
+      expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
     end
   end
 end
--- spec/lib/fitting/cover/json_schema_spec.rb
+++ spec/lib/fitting/cover/json_schema_spec.rb
@@ -6,16 +6,14 @@ RSpec.describe Fitting::Cover::JSONSchema do
   subject { described_class.new(original) }
 
   describe '#combi' do
-    context 'default' do
-      let(:original) { MultiJson.load(File.read('spec/fixtures/required/0.json')) }
-      let(:first_combination) { MultiJson.load(File.read('spec/fixtures/required/1.json')) }
-      let(:second_combination) { MultiJson.load(File.read('spec/fixtures/required/2.json')) }
-      let(:first_details) { %w[required captcha] }
-      let(:second_details) { %w[required code] }
+    let(:original) { MultiJson.load(File.read('spec/fixtures/required/0.json')) }
+    let(:first_combination) { MultiJson.load(File.read('spec/fixtures/required/1.json')) }
+    let(:second_combination) { MultiJson.load(File.read('spec/fixtures/required/2.json')) }
+    let(:first_details) { %w[required captcha] }
+    let(:second_details) { %w[required code] }
 
-      it 'returns combinations' do
-        expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
-      end
+    it 'returns combinations' do
+      expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
     end
 
     context 'attachments' do
