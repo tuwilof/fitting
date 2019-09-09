e3c1fddd1b893ceea86d0215d1a2023c58f302c5
d.efimov <d.efimov@fun-box.ru>
2019-09-09 20:43:41 +0400

    set all of

--- lib/fitting/cover/json_schema_one_of.rb
+++ lib/fitting/cover/json_schema_one_of.rb
@@ -8,11 +8,19 @@ module Fitting
       def combi
         @combinations = []
 
-        return @combinations unless @json_schema['oneOf']
+        return @combinations unless @json_schema['oneOf'] || @json_schema['allOf']
 
-        one_of = @json_schema.delete("oneOf")
-        one_of.each_index do |index|
-          @combinations.push([@json_schema.merge(one_of[index]), ['one_of', "properties.#{index}"]])
+        if @json_schema['oneOf']
+          one_of = @json_schema.delete("oneOf")
+          one_of.each_index do |index|
+            @combinations.push([@json_schema.merge(one_of[index]), ['one_of', "properties.#{index}"]])
+          end
+        elsif @json_schema['allOf']
+          all_of = @json_schema.delete("allOf")
+          one_of = all_of[0].delete("oneOf")
+          one_of.each_index do |index|
+            @combinations.push([@json_schema.merge(one_of[index]), ['one_of', "properties.#{index}"]])
+          end
         end
 
         @combinations
