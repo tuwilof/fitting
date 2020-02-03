442cdd0451879977244633203ab400a32fae2165
d.efimov <d.efimov@fun-box.ru>
2020-02-03 21:26:24 +0400

    add nested one of

--- lib/fitting/cover/json_schema_one_of.rb
+++ lib/fitting/cover/json_schema_one_of.rb
@@ -3,19 +3,33 @@ module Fitting
     class JSONSchemaOneOf
       def initialize(json_schema)
         @json_schema = json_schema
+        @combinations = []
       end
 
       def combi
-        @combinations = []
+        inception(@json_schema, @combinations).each do |combination|
+          combination[0] = @json_schema.merge(combination[0])
+          combination[1] = ['one_of', combination[1]]
+        end
+      end
 
-        return @combinations unless @json_schema['oneOf'] || @json_schema['allOf']
-
-        one_of = @json_schema.delete("oneOf")
-        one_of.each_index do |index|
-          @combinations.push([@json_schema.merge('oneOf' => [one_of[index]]), ['one_of', "#{index}"]])
+      def inception(json_schema, combinations)
+        json_schema.each do |key, value|
+          if key == 'oneOf'
+            one_of = json_schema.delete('oneOf')
+            one_of.each_index do |index|
+              combinations.push([json_schema.merge('oneOf' => [one_of[index]]), "oneOf.#{index}"])
+            end
+          elsif value.is_a?(Hash)
+            inception(value, combinations)
+            combinations.each do |combination|
+              combination[0] = { key => combination[0]}
+              combination[1] = "#{key}.#{combination[1]}"
+            end
+          end
         end
 
-        @combinations
+        combinations
       end
     end
   end
--- spec/lib/fitting/cover/json_schema_one_of_spec.rb
+++ spec/lib/fitting/cover/json_schema_one_of_spec.rb
@@ -10,8 +10,20 @@ RSpec.describe Fitting::Cover::JSONSchemaOneOf do
     let(:original) { MultiJson.load(File.read('spec/fixtures/one_of/0.json')) }
     let(:first_combination) { MultiJson.load(File.read('spec/fixtures/one_of/1.json')) }
     let(:second_combination) { MultiJson.load(File.read('spec/fixtures/one_of/2.json')) }
-    let(:first_details) { %w[one_of 0] }
-    let(:second_details) { %w[one_of 1] }
+    let(:first_details) { %w[one_of oneOf.0] }
+    let(:second_details) { %w[one_of oneOf.1] }
+
+    it 'returns combinations' do
+      expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
+    end
+  end
+
+  describe '#combi nested' do
+    let(:original) { MultiJson.load(File.read('spec/fixtures/one_of/nested_0.json')) }
+    let(:first_combination) { MultiJson.load(File.read('spec/fixtures/one_of/nested_1.json')) }
+    let(:second_combination) { MultiJson.load(File.read('spec/fixtures/one_of/nested_2.json')) }
+    let(:first_details) { %w[one_of properties.errors.oneOf.0] }
+    let(:second_details) { %w[one_of properties.errors.oneOf.1] }
 
     it 'returns combinations' do
       expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
