6474fc80766591cb4be46e96a92c21d73865b2a9
d.efimov <d.efimov@fun-box.ru>
2020-02-03 19:25:46 +0400

    delete all of

--- lib/fitting/cover/json_schema_one_of.rb
+++ lib/fitting/cover/json_schema_one_of.rb
@@ -10,17 +10,9 @@ module Fitting
 
         return @combinations unless @json_schema['oneOf'] || @json_schema['allOf']
 
-        if @json_schema['oneOf']
-          one_of = @json_schema.delete("oneOf")
-          one_of.each_index do |index|
-            @combinations.push([@json_schema.merge('oneOf' => [one_of[index]]), ['one_of', "#{index}"]])
-          end
-        elsif @json_schema['allOf']
-          all_of = @json_schema.delete("allOf")
-          one_of = all_of[0].delete("oneOf")
-          one_of.each_index do |index|
-            @combinations.push([@json_schema.merge(one_of[index]), ['one_of', "properties.#{index}"]])
-          end
+        one_of = @json_schema.delete("oneOf")
+        one_of.each_index do |index|
+          @combinations.push([@json_schema.merge('oneOf' => [one_of[index]]), ['one_of', "#{index}"]])
         end
 
         @combinations
--- spec/lib/fitting/cover/json_schema_one_of_spec.rb
+++ spec/lib/fitting/cover/json_schema_one_of_spec.rb
@@ -17,16 +17,4 @@ RSpec.describe Fitting::Cover::JSONSchemaOneOf do
       expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
     end
   end
-
-  describe '#combi all of' do
-    let(:original) { MultiJson.load(File.read('spec/fixtures/one_of/all_of_0.json')) }
-    let(:first_combination) { MultiJson.load(File.read('spec/fixtures/one_of/all_of_1.json')) }
-    let(:second_combination) { MultiJson.load(File.read('spec/fixtures/one_of/all_of_2.json')) }
-    let(:first_details) { %w[one_of properties.0] }
-    let(:second_details) { %w[one_of properties.1] }
-
-    it 'returns combinations' do
-      expect(subject.combi).to eq([[first_combination, first_details], [second_combination, second_details]])
-    end
-  end
 end
