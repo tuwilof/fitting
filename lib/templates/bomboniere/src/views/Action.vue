963d5f4280ed20048518a2e6f5366a7eb3b51158
d.efimov <d.efimov@fun-box.ru>
2020-08-10 17:56:25 +0400

    add info in action page

--- lib/templates/bomboniere/src/views/Action.vue
+++ lib/templates/bomboniere/src/views/Action.vue
@@ -3,5 +3,20 @@
     Action page
     <p>{{this.$route.query.prefix}}</p>
     <p>{{this.$route.query.method}} {{this.$route.query.path}}</p>
+    {{myJson['prefixes_details']}}
   </div>
 </template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      myJson: {'stub': 'for action page'}
+    }
+  }
+}
+</script>
