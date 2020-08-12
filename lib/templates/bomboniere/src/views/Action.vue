083f1caae8d17c280cf61783dbfa2f27dfcc860c
d.efimov <d.efimov@fun-box.ru>
2020-08-12 17:08:48 +0400

    find action in new page

--- lib/templates/bomboniere/src/views/Action.vue
+++ lib/templates/bomboniere/src/views/Action.vue
@@ -3,7 +3,15 @@
     Action page
     <p>{{this.$route.query.prefix}}</p>
     <p>{{this.$route.query.method}} {{this.$route.query.path}}</p>
-    {{myJson['prefixes_details']}}
+    <div v-for="data in myJson['prefixes_details']">
+      <div v-if="$route.query.prefix == data['name']" class="accordion-item">
+        <div v-for="action in data['actions']['actions_details']">
+          <div v-if="$route.query.method == action['method'] && $route.query.path == action['path']" class="accordion-item">
+            <div class="action">{{action}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,3 +28,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.action {
+  color: #42b983;
+}
+</style>
