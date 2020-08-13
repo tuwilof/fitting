f8ef3842fcc0adf76e2a1817cf3bdf4d1e478dcc
d.efimov <d.efimov@fun-box.ru>
2020-08-13 15:56:40 +0400

    use vue-json-compare

--- lib/templates/bomboniere/src/views/Action.vue
+++ lib/templates/bomboniere/src/views/Action.vue
@@ -3,16 +3,18 @@
     Action page
     <p>{{this.$route.query.prefix}}</p>
     <p>{{this.$route.query.method}} {{this.$route.query.path}}</p>
-    <div v-for="data in myJson['prefixes_details']">
-      <div v-if="$route.query.prefix == data['name']" class="accordion-item">
-        <div v-for="action in data['actions']['actions_details']">
-          <div v-if="$route.query.method == action['method'] && $route.query.path == action['path']" class="accordion-item">
+    <div v-for="data in myJson.prefixes_details">
+      <div v-if="$route.query.prefix == data.name" class="accordion-item">
+        <div v-for="action in data.actions.actions_details">
+          <div v-if="$route.query.method == action.method && $route.query.path == action.path" class="accordion-item">
             <div class="action">
-              <div class="request">{{action['method']}} {{action['path']}}, test {{action['tests_size']}}</div>
-              <div v-for="response in action['responses']['responses_details']">
-                <div class="response">response code {{ response['method'] }}, tests_size {{ response['tests_size'] }}</div>
-                <div v-for="combination in response['combinations']['combinations_details']">
-                  <div class="combination">{{ combination }}</div>
+              <div class="request">{{action.method}} {{action.path}}, test {{action.tests_size}}</div>
+              <div v-for="response in action.responses.responses_details">
+                <div class="response">response code {{ response.method }}, tests_size {{ response.tests_size }}</div>
+                <div v-for="combination in response.combinations.combinations_details">
+                  <div class="combination">
+                    <vue-json-compare :oldData="combination" :newData="combination"></vue-json-compare>
+                  </div>
                 </div>
               </div>
             </div>
@@ -24,10 +26,15 @@
 </template>
 
 <script>
+import vueJsonCompare from 'vue-json-compare'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    vueJsonCompare
   },
   data() {
     return {
@@ -55,5 +62,18 @@ export default {
 .combination {
   background-color: #273645;
   margin: 2px 0px 0px 24px;
+}
+/* darkmode */
+.alpaca-json{
+  background-color: #131a20;
+}
+.alpaca-add{
+  background-color: #004313;
+}
+.alpaca-del {
+  background-color: #160004;
+}
+.alpaca-upd{
+  background-color: #423200;
 }
 </style>
