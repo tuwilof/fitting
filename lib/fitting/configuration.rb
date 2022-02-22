837d1b93d66da7d2b197defc8acb224427a1913e
d.efimov <d.efimov@fun-box.ru>
2022-02-22 17:06:58 +0400

    remove redundant settings

--- lib/fitting/configuration.rb
+++ lib/fitting/configuration.rb
@@ -1,16 +1,12 @@
 module Fitting
   class Configuration
-    attr_accessor :rspec_json_path,
-                  :webmock_json_path,
-                  :fitting_report_path,
+    attr_accessor :fitting_report_path,
                   :prefixes,
                   :outgoing_prefixes
 
     def initialize(config)
-      @rspec_json_path = config['rspec_json_path'] || './fitting_tests'
       @fitting_report_path = config['fitting_report_path'] || './fitting'
       @prefixes = config['prefixes'] || []
-      @webmock_json_path = config['webmock_json_path'] || './outgoing_request_tests'
       @outgoing_prefixes = config['outgoing_prefixes'] || []
     end
   end
