94df62cead1df25158d051922554d49a2a438094
d.efimov <d.efimov@fun-box.ru>
2022-04-13 15:35:32 +0400

    more info

--- lib/fitting/report/responses.rb
+++ lib/fitting/report/responses.rb
@@ -5,6 +5,7 @@ module Fitting
     class Responses
       class NotFound < RuntimeError
         attr_reader :log
+
         def initialize(msg, log)
           @log = log
           super(msg)
@@ -20,13 +21,20 @@ module Fitting
       end
 
       def find!(log)
+        message = ""
         @responses.map do |response|
-          if response.status.to_s == log.status && JSON::Validator.fully_validate(response.body, log.body) == []
-            return response
+          if response.status.to_s == log.status
+            if JSON::Validator.fully_validate(response.body, log.body) == []
+              return response
+            else
+              message += "\nbody: #{response.body}\nvalidate: #{JSON::Validator.fully_validate(response.body, log.body)}\n"
+            end
+          else
+            message += "\nstatus: #{response.status}\n"
           end
         end
 
-        raise NotFound.new("method: #{log.method}, host: #{log.host}, path: #{log.path}, status: #{log.status}, body: #{log.body}", log)
+        raise NotFound.new("method: #{log.method}, host: #{log.host}, path: #{log.path}, status: #{log.status}, body: #{log.body}\n\n#{message}", log)
       end
 
       def to_a
