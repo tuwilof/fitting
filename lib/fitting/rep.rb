bd27bfc066171d483b3467fc1fcede7151d10e8e
d.efimov <d.efimov@fun-box.ru>
2022-05-23 18:01:22 +0400

    add module window for action

--- lib/fitting/rep.rb
+++ lib/fitting/rep.rb
@@ -11,13 +11,15 @@ module Fitting
       gem_path = $LOAD_PATH.find { |i| i.include?('fitting') }
       source_path = "#{gem_path}/templates/htmlcss/fitting.html"
 
+      divs = ""
       res = ""
       @actions = @apis.inject([]) { |res, api| res += api.actions }
       @actions.sort{|a, b| a.cover <=> b.cover }.each do |action|
+        key = SecureRandom.hex
         res +=
           "    <tr>"\
           "        <td>#{action.method}</td>"\
-          "        <td>#{action.host}#{action.prefix}#{action.path}</td>"\
+          "        <td><a href='##{key}'>#{action.host}#{action.prefix}#{action.path}</a></td>"\
           "        <td#{action.cover == 100 ? ' class="green"' : ' class="red"'}>#{action.cover}%</td>"\
           "        <td>89</td>"\
           "        <td>19</td>"\
@@ -25,10 +27,20 @@ module Fitting
           "        <td>10</td>"\
           "        <td>3.79</td>"\
           "    </tr>"
+        divs +=
+          "<div id='#{key}' class='overlay'>"\
+          "    <div class='popup'>"\
+          "        <h2>#{action.host}#{action.prefix}#{action.path}</h2>"\
+          "        <a class='close' href='#'>&times;</a>"\
+          "        <div class='content'>"\
+          "            Thank to pop me out of that button, but now i'm done so you can close this window."\
+          "        </div>"\
+          "    </div>"\
+          "</div>"
       end
 
       file =  File.read(source_path)
-      new_file = file.gsub('<tr></tr>', res)
+      new_file = file.gsub('<tr></tr>', res).gsub('<div></div>', divs)
       File.open('coverage/fitting.html', 'w') { |file| file.write(new_file) }
     end
   end
