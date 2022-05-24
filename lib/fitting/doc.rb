85362a32eb05f1cc65a1f5e483a5b160999ea71d
d.efimov <d.efimov@fun-box.ru>
2022-05-24 15:00:33 +0400

    used one api class

--- lib/fitting/doc.rb
+++ lib/fitting/doc.rb
@@ -1,5 +1,4 @@
-require 'fitting/doc/provided_api'
-require 'fitting/doc/used_api'
+require 'fitting/doc/api'
 
 module Fitting
   class Doc
@@ -8,22 +7,26 @@ module Fitting
     class NotFound < RuntimeError; end
 
     def self.all(yaml)
-      Fitting::Doc::ProvidedAPI.all(yaml) + Fitting::Doc::UsedAPI.all(yaml)
+      yaml['ProvidedAPIs'].map do |host|
+        Fitting::Doc::API.new('provided', 'www.example.com', host['prefix'], host['path'])
+      end + yaml['UsedAPIs'].map do |host|
+        Fitting::Doc::API. new( 'used', host['host'], '', host['path'])
+      end
     end
 
     def self.find!(docs, log)
       docs.find do |doc|
-        if doc.class == Fitting::Doc::ProvidedAPI && log.type == 'incoming'
+        if doc.type == 'provided' && log.type == 'incoming'
           if log.host == doc.host
             doc.prefix['name'].nil? || log.path[0..prefix['name'].size - 1] == doc.prefix['name']
           end
-        elsif doc.class == Fitting::Doc::UsedAPI && log.type == 'outgoing'
+        elsif doc.type == 'used' && log.type == 'outgoing'
           if log.host == doc.host
             doc.prefix['name'].nil? || log.path[0..prefix['name'].size - 1] == doc.prefix['name']
           end
         end
       end&.find!(log)
-    rescue Fitting::Doc::UsedAPI::NotFound, Fitting::Doc::ProvidedAPI::NotFound => e
+    rescue Fitting::Doc::API::NotFound => e
       raise NotFound, e
     end
 
