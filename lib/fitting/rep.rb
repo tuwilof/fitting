adb6ee1445bfa6d21020aacaead16627abe929dc
d.efimov <d.efimov@fun-box.ru>
2022-11-22 17:47:21 +0400

    improve name files

--- lib/fitting/rep.rb
+++ lib/fitting/rep.rb
@@ -13,8 +13,8 @@ module Fitting
 
       fitting_json = Fitting::Rep::JSON.to_s(@actions)
       fitting_lock_json = Fitting::Rep::JSON.lock(@actions)
-      File.open('coverage/fitting.json', 'w') { |file| file.write(::JSON.pretty_generate(fitting_json)) }
-      File.open('coverage/fitting.lock.json', 'w') { |file| file.write(::JSON.pretty_generate(fitting_lock_json)) }
+      File.open('coverage/.fitting.json', 'w') { |file| file.write(::JSON.pretty_generate(fitting_json)) }
+      File.open('coverage/.fitting.lock.json', 'w') { |file| file.write(::JSON.pretty_generate(fitting_lock_json)) }
       Fitting::Rep::HTML.bootstrap('coverage', fitting_json, fitting_lock_json)
     end
   end
