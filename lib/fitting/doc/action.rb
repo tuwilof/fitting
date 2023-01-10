a6e74a34a04aa3cc6de95c835e09d342c4e2e9ae
defimov <defimov@megalabs.ru>
2023-01-10 12:38:15 +0400

    delete don't use variables

--- lib/fitting/doc/action.rb
+++ lib/fitting/doc/action.rb
@@ -3,7 +3,7 @@ require 'fitting/doc/response'
 module Fitting
   class Doc
     class Action
-      attr_accessor :key, :type, :host, :prefix, :path, :method, :responses, :host_cover, :prefix_cover, :path_cover, :method_cover
+      attr_accessor :type, :host, :prefix, :path, :method, :responses
 
       def initialize(type, host, prefix, method, path, responses)
         @type = type
@@ -11,7 +11,6 @@ module Fitting
         @prefix = prefix
         @method = method
         @path = path
-        @cover = 0
         @responses = Fitting::Doc::Response.all(responses)
         @key = "#{method} #{host}#{prefix}#{path}"
 
@@ -19,10 +18,6 @@ module Fitting
         @prefix_cover = 0
         @path_cover = 0
         @method_cover = 0
-      end
-
-      def url
-        "#{@host}#{@prefix}#{@path}"
       end
 
       def to_hash_lock
@@ -48,10 +43,10 @@ module Fitting
       def to_hash
         res = []
         (to_hash_lock[@key ].size).times { res.push(nil) }
-        res[1] = host_cover
-        res[2] = prefix_cover
-        res[3] = path_cover
-        res[4] = method_cover
+        res[1] = @host_cover
+        res[2] = @prefix_cover
+        res[3] = @path_cover
+        res[4] = @method_cover
         { @key => res }
       end
 
@@ -91,36 +86,27 @@ module Fitting
         unless log.host == host
           return
         end
-        @cover = 25 if @cover < 25
         @host_cover += 1
 
         unless prefix.size == 0 || log.path[0..prefix.size - 1] == prefix
           return
         end
-        @cover = 50 if @cover < 50
         @prefix_cover += 1
 
         unless path_match(log.path)
           return
         end
-        @cover = 75 if @cover < 75
         @path_cover += 1
 
         unless log.method == method
           return
         end
-        @cover = 100 if @cover < 100
         @method_cover += 1
 
         true
       end
 
-      def cover
-        @cover
-      end
-
       def nocover!
-        @cover = 100
         @host_cover = nil
         @prefix_cover = nil
         @path_cover = nil
