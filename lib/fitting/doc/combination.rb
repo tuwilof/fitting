c8dc9affdbb8346f3a3e86f882cef7a1bc3d6749
defimov <defimov@megalabs.ru>
2023-01-13 16:08:47 +0400

    fix definitions

--- lib/fitting/doc/combination.rb
+++ lib/fitting/doc/combination.rb
@@ -7,20 +7,22 @@ module Fitting
 
       def initialize(json_schema, type, combination)
         @step_cover_size = 0
-        @json_schema = json_schema
+        @step_key = json_schema
         @type = type
         @combination = combination
       end
 
       def cover!(log)
-        if JSON::Validator.fully_validate(@json_schema, log.body) == []
+        if JSON::Validator.fully_validate(@step_key, log.body) == []
           @step_cover_size += 1
+=begin
         else
           raise NotFound.new "combination: #{@combination}\n"\
             "combination type: #{@type}\n"\
-            "combination json-schema: #{::JSON.pretty_generate(@json_schema)}\n"\
-            "combination error #{::JSON.pretty_generate(JSON::Validator.fully_validate(@json_schema, log.body))}\n"\
+            "combination json-schema: #{::JSON.pretty_generate(@step_key)}\n"\
+            "combination error #{::JSON.pretty_generate(JSON::Validator.fully_validate(@step_key, log.body))}\n"\
             "body: #{::JSON.pretty_generate(log.body)}"
+=end
         end
       end
     end
--- lib/fitting/doc/content_type.rb
+++ lib/fitting/doc/content_type.rb
@@ -12,28 +12,60 @@ module Fitting
         if subvalue.size == 1
           @next_steps.push(JsonSchema.new(subvalue[0]['body']))
         else
-          @next_steps.push(JsonSchema.new(
-            {
-              "$schema" => "http://json-schema.org/draft-04/schema#",
-              "type" => "object",
-              "oneOf" => subvalue.inject([]) do |sum, sv|
-                if sv['body']["required"] == nil
-                  sum.push(
-                    {
-                      "properties" => sv['body']["properties"]
-                    }
-                  )
-                else
-                  sum.push(
-                    {
-                      "properties" => sv['body']["properties"],
-                      "required" => sv['body']["required"]
-                    }
-                  )
+          definitions = subvalue.inject({}) do |sum, sv|
+            if sv['body']["definitions"] != nil
+              sum.merge!(sv['body']["definitions"])
+            end
+            sum
+          end
+          if definitions
+            @next_steps.push(JsonSchema.new(
+              {
+                "$schema" => "http://json-schema.org/draft-04/schema#",
+                "definitions" => definitions,
+                "type" => "object",
+                "oneOf" => subvalue.inject([]) do |sum, sv|
+                  if sv['body']["required"] == nil
+                    sum.push(
+                      {
+                        "properties" => sv['body']["properties"]
+                      }
+                    )
+                  else
+                    sum.push(
+                      {
+                        "properties" => sv['body']["properties"],
+                        "required" => sv['body']["required"]
+                      }
+                    )
+                  end
                 end
-              end
-            }
-          ))
+              }
+            ))
+          else
+            @next_steps.push(JsonSchema.new(
+              {
+                "$schema" => "http://json-schema.org/draft-04/schema#",
+                "type" => "object",
+                "oneOf" => subvalue.inject([]) do |sum, sv|
+                  if sv['body']["required"] == nil
+                    sum.push(
+                      {
+                        "properties" => sv['body']["properties"]
+                      }
+                    )
+                  else
+                    sum.push(
+                      {
+                        "properties" => sv['body']["properties"],
+                        "required" => sv['body']["required"]
+                      }
+                    )
+                  end
+                end
+              }
+            ))
+          end
         end
       end
 
