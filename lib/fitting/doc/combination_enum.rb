d9d531dd02b4e39fc573e29017795e66c93f2097
defimov <defimov@megalabs.ru>
2023-01-18 16:48:50 +0400

    added enum cover

--- lib/fitting/doc/combination_enum.rb
+++ lib/fitting/doc/combination_enum.rb
@@ -1,10 +1,9 @@
 require 'fitting/doc/step'
 require 'fitting/cover/json_schema_enum'
-require 'fitting/doc/combination_enum'
 
 module Fitting
   class Doc
-    class Combination < Step
+    class CombinationEnum < Step
       class NotFound < RuntimeError; end
 
       def initialize(json_schema, type, combination)
@@ -13,18 +12,11 @@ module Fitting
         @next_steps = []
         @type = type
         @step_key = combination
-        combinations = Fitting::Cover::JSONSchemaEnum.new(@json_schema).combi
-        if combinations.size > 1
-          combinations.each do |comb|
-            @next_steps.push(CombinationEnum.new(comb[0], "#{type}.#{comb[1][0]}", "#{combination}.#{comb[1][1]}"))
-          end
-        end
       end
 
       def cover!(log)
         if JSON::Validator.fully_validate(@json_schema, log.body) == []
           @step_cover_size += 1
-          @next_steps.each { |combination| combination.cover!(log) }
 =begin
         else
           raise NotFound.new "combination: #{@combination}\n"\
