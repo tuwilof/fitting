ebcb881564b9345cee32ff7239cbed7541bc2f4d
defimov <defimov@megalabs.ru>
2023-01-23 17:47:05 +0400

    improved more

--- lib/fitting/debug.rb
+++ lib/fitting/debug.rb
@@ -15,32 +15,14 @@ module Fitting
         debug = new(action['host'], action['method'], action['path'], action['code'], action['content-type'])
         res = Fitting::Doc.debug(docs, debug)
         doc_to_hash_lock = []
-        res.report(doc_to_hash_lock, 0)
+        res.next_steps.first.report(doc_to_hash_lock, 0)
         res_debug = {
-          doc_to_hash: res.to_hash,
-          doc_to_hash_lock: doc_to_hash_lock,
-          res_logs: res.logs
+          json_schema: res.next_steps.first.to_hash,
+          lock: doc_to_hash_lock,
+          logs: res.logs
         }
         File.open('coverage/.fitting.debug.json', 'w') { |file| file.write(::JSON.pretty_generate(res_debug)) }
       end
-
-      # res_logs = []
-      # res_debug = []
-      # res_docs = []
-      # return [] unless yaml['Debug']
-      # yaml['Debug'].map do |action|
-      #   debug = new(action['host'], action['method'], action['path'], action['code'], action['content-type'])
-      #   logs.each do |log|
-      #     doc, res_log = Fitting::Doc.debug(docs, log, debug)
-      #     res_docs.push(doc) if doc
-      #     res_logs.push(res_log) if res_log
-      #   end
-      #   if docs
-      #     doc = res_docs.first
-      #     res_debug.push({doc_to_hash: doc.to_hash, doc_to_hash_lock: doc.to_hash_lock, res_logs: res_logs})
-      #   end
-      # end
-      # File.open('coverage/.fitting.debug.json', 'w') { |file| file.write(::JSON.pretty_generate(res_debug)) }
     end
   end
 end
