6b9c8608e7f1fd7f52aee069ef0f243461a08b17
defimov <defimov@megalabs.ru>
2023-01-24 18:48:43 +0400

    add new test

--- lib/fitting/debug.rb
+++ lib/fitting/debug.rb
@@ -16,6 +16,7 @@ module Fitting
         debug = new(action['host'], action['method'], action['path'], action['code'], action['content-type'])
         res = Fitting::Doc.debug(docs, debug)
         combinations = []
+        index = res.next_steps.first.index_before
         res.next_steps.first.next_steps.each do |next_step|
           combinations.push(
             {
@@ -23,11 +24,11 @@ module Fitting
               "combination" => next_step.step_key,
               "json_schema" => next_step.json_schema,
               "valid_jsons" => next_step.logs,
-              "index_before" => next_step.index_before - 7,
-              "index_after" => next_step.index_after - 7,
-              "res_before" => next_step.res_before.map{|r| r ? r : "null"}[7..-1],
-              "res_medium" => next_step.res_medium.map{|r| r ? r : "null"}[7..-1],
-              "res_after" => next_step.res_after.map{|r| r ? r : "null"}[7..-1]
+              "index_before" => next_step.index_before - index,
+              "index_after" => next_step.index_after - index,
+              "res_before" => next_step.res_before.map{|r| r ? r : "null"}[index..-1],
+              "res_medium" => next_step.res_medium.map{|r| r ? r : "null"}[index..-1],
+              "res_after" => next_step.res_after.map{|r| r ? r : "null"}[index..-1]
             }
           )
 
@@ -36,11 +37,11 @@ module Fitting
           "json_schema" => res.next_steps.first.to_hash,
           "jsons" => res.logs,
           "valid_jsons" => res.next_steps.first.logs,
-          "index_before" => res.next_steps.first.index_before - 7,
-          "index_after" => res.next_steps.first.index_after - 7,
-          "res_before" => res.next_steps.first.res_before.map{|r| r ? r : "null"}[7..-1],
-          "res_medium" => res.next_steps.first.res_medium.map{|r| r ? r : "null"}[7..-1],
-          "res_after" => res.next_steps.first.res_after.map{|r| r ? r : "null"}[7..-1],
+          "index_before" => res.next_steps.first.index_before - index,
+          "index_after" => res.next_steps.first.index_after - index,
+          "res_before" => res.next_steps.first.res_before.map{|r| r ? r : "null"}[index..-1],
+          "res_medium" => res.next_steps.first.res_medium.map{|r| r ? r : "null"}[index..-1],
+          "res_after" => res.next_steps.first.res_after.map{|r| r ? r : "null"}[index..-1],
           "combinations" => combinations
         }
         File.open('coverage/.fitting.debug.yml', 'w') { |file| file.write(YAML.dump(res_debug)) }
--- spec/lib/fitting/doc/json_schema_spec.rb
+++ spec/lib/fitting/doc/json_schema_spec.rb
@@ -2,16 +2,31 @@ require 'rspec'
 require 'fitting/doc/json_schema'
 
 describe Fitting::Doc::JsonSchema do
-  let(:fixture) { YAML.load(File.read('spec/fixtures/json_schema/.fitting.debug.yml')) }
-
   subject(:json_schema) { described_class.new(fixture["json_schema"]) }
 
-  it do
-    fixture["jsons"].each do |log|
-      json_schema.cover!(double(body:log))
+  context 'fixtures/json_schema/' do
+    let(:fixture) { YAML.load(File.read('spec/fixtures/json_schema/.fitting.debug.yml')) }
+
+    it 'report eq res_after' do
+      fixture["jsons"].each do |log|
+        json_schema.cover!(double(body: log))
+      end
+      doc_to_hash_lock = []
+      res = json_schema.report(doc_to_hash_lock, 0)
+      expect(res[0].map { |r| r ? r : "null" }).to eq(fixture["res_after"])
     end
-    doc_to_hash_lock = []
-    res = json_schema.report(doc_to_hash_lock, 0)
-    expect(res[0].map{|r| r ? r : "null"}).to eq(fixture["res_after"])
+  end
+
+  context 'fixtures/json_schema_1' do
+    let(:fixture) { YAML.load(File.read('spec/fixtures/json_schema_1/.fitting.debug.yml')) }
+
+    it 'report eq res_after' do
+      fixture["jsons"].each do |log|
+        json_schema.cover!(double(body: log))
+      end
+      doc_to_hash_lock = []
+      res = json_schema.report(doc_to_hash_lock, 0)
+      expect(res[0].map { |r| r ? r : "null" }).to eq(fixture["res_after"])
+    end
   end
 end
