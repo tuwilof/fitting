1d495557c3b5bc37ffa0864483bb7668e2b05d35
defimov <defimov@megalabs.ru>
2023-01-25 12:47:10 +0400

    added new test

--- spec/lib/fitting/doc/json_schema_spec.rb
+++ spec/lib/fitting/doc/json_schema_spec.rb
@@ -6,10 +6,10 @@ require 'byebug'
 describe Fitting::Doc::JsonSchema do
   subject(:json_schema) { described_class.new(fixture["json_schema"]) }
 
-  context 'fixtures/json_schema/' do
-    let(:fixture) { YAML.load(File.read('spec/fixtures/json_schema/.fitting.debug.yml')) }
+  context 'bug 1' do
+    let(:fixture) { YAML.load(File.read('spec/fixtures/bugs/bug1/.fitting.debug.yml')) }
 
-    it 'report eq res_after' do
+    it 'fixed' do
       fixture["jsons"].each do |log|
         json_schema.cover!(double(body: log))
       end
@@ -19,8 +19,21 @@ describe Fitting::Doc::JsonSchema do
     end
   end
 
-  context 'fixtures/json_schema_1' do
-    let(:fixture) { YAML.load(File.read('spec/fixtures/json_schema_1/.fitting.debug.yml')) }
+  context 'bug 2' do
+    let(:fixture) { YAML.load(File.read('spec/fixtures/bugs/bug2/.fitting.debug.yml')) }
+
+    it 'fixed' do
+      fixture["jsons"].each do |log|
+        json_schema.cover!(double(body: log))
+      end
+      doc_to_hash_lock = []
+      res = json_schema.report(doc_to_hash_lock, 0)
+      expect(res[0].map { |r| r ? r : "null" }).to eq(fixture["res_after"])
+    end
+  end
+
+  context 'bug 3' do
+    let(:fixture) { YAML.load(File.read('spec/fixtures/bugs/bug2/.fitting.debug.yml')) }
 
     it 'report eq res_after' do
       fixture["jsons"].each do |log|
