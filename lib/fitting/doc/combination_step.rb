cd4cb0da166261036ea5b216e659500577437fff
defimov <defimov@megalabs.ru>
2023-01-27 10:25:02 +0400

    more combinations

--- lib/fitting/doc/combination_step.rb
+++ lib/fitting/doc/combination_step.rb
@@ -4,6 +4,12 @@ module Fitting
   class Doc
     class CombinationStep < Step
       def debug_report(index)
+        combinations = []
+        @next_steps.each do |next_step|
+          combinations.push(
+            next_step.debug_report(index)
+          )
+        end
         {
           "combination type" => @type,
           "combination" => @step_key,
@@ -14,7 +20,8 @@ module Fitting
           "index_after" => @index_after - index,
           "res_before" => @res_before.map{|r| r ? r : "null"}[index..-1],
           "res_medium" => @res_medium.map{|r| r ? r : "null"}[index..-1],
-          "res_after" => @res_after.map{|r| r ? r : "null"}[index..-1]
+          "res_after" => @res_after.map{|r| r ? r : "null"}[index..-1],
+          "combinations" => combinations
         }
       end
     end
